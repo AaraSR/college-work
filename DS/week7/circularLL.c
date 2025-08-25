763dda27ef678c0b6126ffba87259756efe2e201
Aaradhya Raut <aaradhya1402@gmail.com>
2025-08-25 04:18:27 +0000

    upd2 of w7

--- DS/week7/circularLL.c
+++ DS/week7/circularLL.c
@@ -1,16 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef struct {
+// define struct Node
+struct Node {
     int data;
-    struct Node* prev;
     struct Node* next;
-} Node;
+};
 
-void createNode(int data) {
-    
+// createNode func
+struct Node* createNode(int new_data) {
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+
+    new_node->data = new_data;
+    new_node->next = NULL;
+
+    return new_node;
 }
 
-int main(void) {
+// insert at 'pos'
+struct Node* insertAtPos(struct Node *last, int data, int pos) {
+    if (last == NULL) {
+        if (pos != 1) {
+            printf("Invalid position!\n");
+            return last;
+        }
 
-    return 0;
+        struct Node *newNode = createNode(data);
+        last = newNode;
+        last->next = last;
+
+        return last;
+    }
+
+    struct Node *newNode = createNode(data);
+
+    struct Node *curr = last->next;
+
+    // when pos = 1, Node->
+    if (pos == 1) {
+        newNode->next = curr;
+        last->next = newNode;
+
+        return last;
+    }
+
+    // traverse to reach pos
 }
