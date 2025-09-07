d0e168668e90257473088f68a9532487543d52e7
Aaradhya Raut <aaradhya1402@gmail.com>
2025-09-07 17:47:36 +0000

    completed Circular LL

--- DS/week7/circularLL.c
+++ DS/week7/circularLL.c
@@ -18,13 +18,44 @@ struct Node* createNode(int new_data) {
 }
 
 // travelling and printing/displaying using iteration
-void display(struct Node* head) {
-    if (head->data == NULL) {
-        
+void traverseList(struct Node* head) {
+    if (head == NULL) {
+        printf("Linked List is empty!\n");
+        return;
     }
+    struct Node* temp = head;
+    printf("HEAD -> ");
+    do {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    } while (temp != head);
+    printf("HEAD\n");
+}
+
+// inserting node at end and linking it to HEAD
+void insertEnd(struct Node** head_ref, int new_data) {
+    struct Node* newNode = createNode(new_data);
+    if (*head_ref == NULL) {
+        *head_ref = newNode;
+        newNode->next = newNode;  // linking to itself when LL is empty
+        return;
+    }
+    struct Node* temp = *head_ref;
+    while (temp->next != *head_ref) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    newNode->next = *head_ref;
 }
 
 int main(void) {
+    struct Node* head = NULL;
+
+    insertEnd(&head, 10);
+    insertEnd(&head, 20);
+    insertEnd(&head, 30);
+
+    traverseList(head);
 
     return 0;
 }
