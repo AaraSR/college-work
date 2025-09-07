66d839f5f8c8b9621d743e0ced9332e55d18324c
Aaradhya Raut <aaradhya1402@gmail.com>
2025-09-07 07:37:07 +0000

    last commit be4 mse, maybe

--- DS/week6/linkedlist.c
+++ DS/week6/linkedlist.c
@@ -18,22 +18,20 @@ struct Node* createNode(int new_data) {
 }
 
 // func to add a node
-void addNode(struct Node** head_ref, int new_data) {
+struct Node* addNode(struct Node* head, int new_data) {
     struct Node* new_node = createNode(new_data);
-    struct Node* last = *head_ref;
-
-    // if the 'next' ptr has NULL, create a new node 
-    if (*head_ref == NULL) {
-        *head_ref = new_node;
-        return;
+    if (head == NULL) {
+        // If list is empty, new node becomes head
+        return new_node;
+    } else {
+        // Otherwise, traverse to the end and add the new node
+        struct Node* temp = head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = new_node;
+        return head;
     }
-
-    // if the 'next' ptr has other node, go on (while loop) till we reach the last node
-    while (last->next != NULL) {
-        last = last->next;
-    }
-    // then create a new node there
-    last->next = new_node;
 }
 
 // // traversing & printing LL using Recursion
